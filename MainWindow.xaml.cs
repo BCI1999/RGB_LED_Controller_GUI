11e94d9c853884b7cb06f5c6f9396f14d10c19dc
BCI1999 <ian-huysman@live.nl>
2022-02-11 17:46:20 +0100

    Temporary fixes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -34,8 +34,6 @@ namespace RGB_LED_Controller
         byte GreenValue;
         byte BlueValue;
 
-        bool exectue = false;
-
         //Making serial port
         SerialPort _SerialPort;
 
@@ -49,7 +47,7 @@ namespace RGB_LED_Controller
             _SerialPort = new SerialPort();
             _SerialPort.BaudRate = 57600;
 
-            //Dsipatch timer
+            //Dsipatcher timer
             _dispatcherTimer = new DispatcherTimer();
             _dispatcherTimer.Interval = TimeSpan.FromMilliseconds(5);
             _dispatcherTimer.Tick += _dispatcherTimer_Tick;
@@ -59,7 +57,7 @@ namespace RGB_LED_Controller
             cbxEffect.Items.Add("Cycle");       //Index 1
 
             rainbow = new Cycle_Effect();
-        }    
+        }
 
         //Adding serial port names to the combo box
         private void ProgramLoaded(object sender, RoutedEventArgs e)
@@ -85,12 +83,11 @@ namespace RGB_LED_Controller
                     rainbow.RGB_Cycle(RedValue, GreenValue, BlueValue);
                     RedValue = rainbow.Red;
                     GreenValue = rainbow.Green;
-                    BlueValue = rainbow.Blue;                    
+                    BlueValue = rainbow.Blue;
                 break;
             }
-
-            //Send the actual data, with their respective ASCII reference bytes (R, G and B)
-            //And send an "end" byte (255) for synchronisation 
+            
+            //Send RGB values and an "end" byte (255) for synchronisation 
             //The RGB value sent by the program may not exceed 254!
             byte[] RGBdata = { RedValue, GreenValue, BlueValue, 0xFF };
             if (_SerialPort.IsOpen)
@@ -104,7 +101,8 @@ namespace RGB_LED_Controller
 
         private void RED_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            SliderRed = Convert.ToByte(RED.Value);
+            //SliderRed = Convert.ToByte(RED.Value);
+            rainbow.Red = Convert.ToByte(RED.Value);
         }
 
         private void GREEN_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -131,7 +129,7 @@ namespace RGB_LED_Controller
             }
         }
 
-        //Method to put RGB slider values onto the preview
+        //Method to put RGB values onto the preview
         private void RGB(byte RED, byte GREEN, byte BLUE, Rectangle rectangle)
         {
             Color RGBColor = new Color();
@@ -139,6 +137,6 @@ namespace RGB_LED_Controller
             SolidColorBrush rgb = new SolidColorBrush();
             rgb.Color = RGBColor;
             rectangle.Fill = rgb;
-        }        
+        }
     }
 }
